334d47e6e81ed6a7cd053c4c48d6dff11759acfa
Norvicdev <101652762+Norvicdev@users.noreply.github.com>
2026-03-20 13:23:14 +0300

    created controller, pushed remote

---
+++ src/main/java/com/vicsergeev/WalletManager/controller/WalletController.java
@@ -0,0 +1,12 @@
+package com.vicsergeev.WalletManager.controller;
+
+/*
+ * Created by Vic
+ * 20.03.2026
+ */
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WalletController {
+}
